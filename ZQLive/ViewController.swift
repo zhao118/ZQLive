2658cd6a3e39db2cd45ec02aff68bee9ed5fc2a5
赵强 <568423952@qq.com>
2021-11-21 17:36:10 +0800

    Initial Commit

---
+++ ZQLive/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ZQLive
+//
+//  Created by ZhaoQ on 2021/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
